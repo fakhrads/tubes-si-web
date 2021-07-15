040dfe61de6e609fa6b1b24650d43151a4615356
Fakhri Adi Saputra <fakhriadisaputra15@gmail.com>
2021-07-15 17:46:26 +0700

    Add Contoller & View

---
+++ resources/views/anggota/create.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+
+@section('content')
+    Halaman Tambah Anggota
+@endsection
---
+++ resources/views/buku/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+
+@section('content')
+    Halaman Buku
+@endsection
