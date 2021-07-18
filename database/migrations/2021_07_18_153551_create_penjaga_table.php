446052aa80fb3fb70935fcf7cd091719dfb0cb52
Fakhri Adi Saputra <fakhriadisaputra15@gmail.com>
2021-07-18 22:36:24 +0700

    Preparing the database

---
+++ database/migrations/2021_07_18_153551_create_penjaga_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenjagaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penjaga', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penjaga');
+    }
+}
