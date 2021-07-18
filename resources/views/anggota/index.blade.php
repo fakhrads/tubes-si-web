54c6562b1f6f3c8f1afe330741eb0f1c87cca1c1
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2021-07-18 16:50:59 +0700

    update tabel, tambah, edit anggota | tambah,edit buku (imsan)

--- resources/views/anggota/index.blade.php
+++ resources/views/anggota/index.blade.php
@@ -3,6 +3,7 @@
 @section('customcss')
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/tables/datatable/datatables.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/pickers/pickadate/pickadate.css') }}">
 @endsection
 
 @section('customscripts')
@@ -16,6 +17,12 @@
     <script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js') }}"></script>
     <script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('app-assets/js/scripts/datatables/datatable.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/ui/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.date.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.time.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/legacy.js') }}"></script>
+    <script src="{{ asset('app-assets/js/scripts/pickers/dateTime/pick-a-datetime.js') }}"></script>
 @endsection
 
 @section('content')
@@ -23,25 +30,54 @@
 <section id="basic-datatable">
     <div class="row">
         <div class="col-12">
+            <div class="text-right">
+                <button type="button" class="btn btn-primary mr-1 mb-1" data-toggle="modal" data-target="#default">Tambah Data</button>
+            </div>
+
+        <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                    <h4 class="card-title">asd</h4>
+                    <h4 class="card-title">Data Anggota</h4>
                 </div>
                 <div class="card-content">
                     <div class="card-body card-dashboard">
-                        <p class="card-text">sad.</p>
                         <div class="table-responsive">
                             <table class="table zero-configuration">
                                 <thead>
                                     <tr>
+                                        <th>ID Anggota</th>
+                                        <th>Nama</th>
+                                        <th>No Telepon</th>
+                                        <th>TTL</th>
+                                        <th>Alamat</th>
+                                        <th>Email</th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                     <tr>
+                                        <td>K01</td>
+                                        <td>Kedokteran Gigi</td>
+                                        <td>Saeful Anwar</td>
+                                        <td>Informatika</td>
+                                        <td>2001</td>
+                                        <td>July-01-2001</td>
+                                        <td>
+                                            <button type="button" class="btn btn-icon btn-outline-primary mr-1 mb-1" data-toggle="modal" data-target="#default2"><i class="feather icon-edit-1"></i></button>
+                                            <button type="button" class="btn btn-icon btn-outline-danger mr-1 mb-1"><i class="feather icon-trash"></i></button>
+                                        </td>
                                     </tr>
                                 </tbody>
                                 <tfoot>
                                     <tr>
+                                        <th>ID Anggota</th>
+                                        <th>Nama</th>
+                                        <th>No Telepon</th>
+                                        <th>TTL</th>
+                                        <th>Alamat</th>
+                                        <th>Email</th>
+                                        <th></th>
+                                    </tr>
                                 </tfoot>
                             </table>
                         </div>
@@ -51,4 +87,115 @@
         </div>
     </div>
 </section>
+<!-- Modal content-->
+<div class="modal fade text-left" id="default" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel1">Tambah Anggota</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="form form-vertical">
+                    <div class="form-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="Nama">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="No Telp">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <form>
+                                        <input type='text' class="form-control format-picker" placeholder="Tempat Tanggal Lahir">
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="password-vertical" class="form-control" name="fname" placeholder="Alamat">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="password-vertical" class="form-control" name="contact" placeholder="Email">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <div class="col-12">
+                    <div class="text-right">
+                        <button type="button" class="btn bg-gradient-danger mr-1 mb-1" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn bg-gradient-primary mr-1 mb-1">Tambah Data</button>
+                    </div>
+                </form>
+                </div>   
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade text-left" id="default2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-scrollable" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="myModalLabel1">Edit Anggota</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form class="form form-vertical">
+                    <div class="form-body">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="Nama">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="No Telp">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <form>
+                                        <input type='text' class="form-control format-picker" placeholder="Tempat Tanggal Lahir">
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="password-vertical" class="form-control" name="fname" placeholder="Alamat">
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <input type="text" id="password-vertical" class="form-control" name="contact" placeholder="Email">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+            <div class="modal-footer">
+                <div class="col-12">
+                    <div class="text-right">
+                        <button type="button" class="btn bg-gradient-danger mr-1 mb-1" data-dismiss="modal">Batal</button>
+                        <button type="submit" class="btn bg-gradient-primary mr-1 mb-1">Edit Data</button>
+                    </div>
+                </form>
+                </div>   
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -3,6 +3,7 @@
 @section('customcss')
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/vendors.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/tables/datatable/datatables.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('app-assets/vendors/css/pickers/pickadate/pickadate.css') }}">
 @endsection
 
 @section('customscripts')
@@ -16,6 +17,12 @@
     <script src="{{ asset('app-assets/vendors/js/tables/datatable/buttons.bootstrap.min.js') }}"></script>
     <script src="{{ asset('app-assets/vendors/js/tables/datatable/datatables.bootstrap4.min.js') }}"></script>
     <script src="{{ asset('app-assets/js/scripts/datatables/datatable.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/ui/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.date.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/picker.time.js') }}"></script>
+    <script src="{{ asset('app-assets/vendors/js/pickers/pickadate/legacy.js') }}"></script>
+    <script src="{{ asset('app-assets/js/scripts/pickers/dateTime/pick-a-datetime.js') }}"></script>
 @endsection
 
 @section('content')
@@ -24,7 +31,7 @@
     <div class="row">
         <div class="col-12">
             <div class="text-right">
-                <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#default">Open Modal</button>
+                <button type="button" class="btn btn-primary mr-1 mb-1" data-toggle="modal" data-target="#default">Tambah Data</button>
             </div>
             
             <div class="card">
@@ -33,7 +40,6 @@
                 </div>
                 <div class="card-content">
                     <div class="card-body card-dashboard">
-                        <p class="card-text">Pada tampilan untuk memuat seluruh data rak buku.</p>
                         <div class="table-responsive">
                             <table class="table zero-configuration">
                                 <thead>
@@ -54,9 +60,9 @@
                                         <td>Saeful Anwar</td>
                                         <td>Informatika</td>
                                         <td>2001</td>
-                                        <td>01-01-2001</td>
+                                        <td>July-01-2001</td>
                                         <td>
-                                            <button type="button" class="btn btn-icon btn-outline-primary mr-1 mb-1"><i class="feather icon-edit-1"></i></button>
+                                            <button type="button" class="btn btn-icon btn-outline-primary mr-1 mb-1" data-toggle="modal" data-target="#default2"><i class="feather icon-edit-1"></i></button>
                                             <button type="button" class="btn btn-icon btn-outline-danger mr-1 mb-1"><i class="feather icon-trash"></i></button>
                                         </td>
                                     </tr>
@@ -85,19 +91,108 @@
         <div class="modal-dialog modal-dialog-scrollable" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h4 class="modal-title" id="myModalLabel1">Basic Modal</h4>
+                    <h4 class="modal-title" id="myModalLabel1">Tambah Buku</h4>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
                 <div class="modal-body">
-                    <h5>Check First Paragraph</h5>
-                    <p>Oat cake ice cream candy chocolate cake chocolate cake cotton candy dragée apple pie.
-                        Brownie carrot cake candy canes bonbon fruitcake topping halvah. Cake sweet roll cake
-                        cheesecake cookie chocolate cake liquorice.</p>
+                    <form class="form form-vertical">
+                        <div class="form-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="Judul Buku">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="Penulis">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <form>
+                                            <input type='text' class="form-control format-picker" placeholder="Tanggal Masuk">
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="password-vertical" class="form-control" name="fname" placeholder="Penerbit">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="password-vertical" class="form-control" name="contact" placeholder="Tahun Terbit">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" data-dismiss="modal">Accept</button>
+                    <div class="col-12">
+                        <div class="text-right">
+                            <button type="button" class="btn bg-gradient-danger mr-1 mb-1" data-dismiss="modal">Batal</button>
+                            <button type="submit" class="btn bg-gradient-primary mr-1 mb-1">Tambah Data</button>
+                        </div>
+                    </form>
+                    </div>   
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal fade text-left" id="default2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title" id="myModalLabel1">Edit Buku</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form class="form form-vertical">
+                        <div class="form-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="Judul Buku">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="first-name-vertical" class="form-control" name="fname" placeholder="Penulis">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <form>
+                                            <input type='text' class="form-control format-picker" placeholder="Tanggal Masuk">
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="password-vertical" class="form-control" name="fname" placeholder="Penerbit">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <div class="form-group">
+                                        <input type="text" id="password-vertical" class="form-control" name="contact" placeholder="Tahun Terbit">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+                <div class="modal-footer">
+                    <div class="col-12">
+                        <div class="text-right">
+                            <button type="button" class="btn bg-gradient-danger mr-1 mb-1" data-dismiss="modal">Batal</button>
+                            <button type="submit" class="btn bg-gradient-primary mr-1 mb-1">Edit Data</button>
+                        </div>
+                    </form>
+                    </div>   
                 </div>
             </div>
         </div>
