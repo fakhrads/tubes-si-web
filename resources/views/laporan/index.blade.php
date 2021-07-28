3468ed22cc7fa9aaf305cf5960c2d1fa3dad740e
IkhsanNurulRizki <ikhsan.n.rizki@gmail.com>
2021-07-28 22:39:14 +0700

    page laporan

--- resources/views/laporan/index.blade.php
+++ resources/views/laporan/index.blade.php
@@ -34,7 +34,40 @@
                 </div>
                 <div class="card-content">
                     <div class="card-body card-dashboard">
-                        Testing
+                        <form class="form form-vertical">
+                            <div class="form-body">
+                                <div class="row">
+                                    <div class="col-9">
+                                        <div class="form-group">
+                                            Bulan pake select 
+                                        </div>
+                                    </div>
+                                    <div class="col-9">
+                                        <div class="form-group">
+                                            <input type="email" id="email-id-vertical" class="form-control" name="email-id" placeholder="Total Kunjungan">
+                                        </div>
+                                    </div>
+                                    <div class="col-9">
+                                        <div class="form-group">
+                                            <input type="number" id="contact-info-vertical" class="form-control" name="contact" placeholder="Total Peminjaman">
+                                        </div>
+                                    </div>
+                                    <div class="col-9">
+                                        <div class="form-group">
+                                            <input type="password" id="password-vertical" class="form-control" name="contact" placeholder="Total Pencarian">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="text-right">
+                                            <button type="submit" class="btn btn-primary mr-1 mb-1">Buat</button>
+                                            <button type="submit" class="btn btn-primary mr-1 mb-1">Kembali</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
                     </div>
                 </div>
             </div>
